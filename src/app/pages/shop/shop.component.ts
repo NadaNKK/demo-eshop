fa144d29cf3be29cf4186f5e3f1cc80eb6d97765
Mahmoud Elkhayat <Elkhayat.mahmoud@outlook.com>
2025-02-15 23:51:49 +0200

    products list

--- src/app/pages/shop/shop.component.ts
+++ src/app/pages/shop/shop.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { SubHeaderComponent } from "../../shared/components/sub-header/sub-header.component";
+import { ProductsListComponent } from "../../shared/components/products-list/products-list.component";
 
 @Component({
   selector: 'app-shop',
   standalone: true,
-  imports: [SubHeaderComponent],
+  imports: [SubHeaderComponent, ProductsListComponent],
   templateUrl: './shop.component.html',
   styleUrl: './shop.component.scss'
 })
