3ae8fc696a0f2713232fa42f8c73805626a03738
Mahmoud Elkhayat <Elkhayat.mahmoud@outlook.com>
2025-02-16 22:51:00 +0200

    product list

--- src/app/shared/components/products-list/products-list.component.ts
+++ src/app/shared/components/products-list/products-list.component.ts
@@ -1,12 +1,62 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-products-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './products-list.component.html',
   styleUrl: './products-list.component.scss'
 })
 export class ProductsListComponent {
-
+ productList=[
+  {
+    "name": "Nike Air Max",
+    "price": 129,
+    "size": 'M',
+    "imageUrl": "images/model.jpg"
+  },
+  {
+    "name": "Adidas Superstar",
+    "price": 99,
+    "size": 'L',
+    "imageUrl": "images/model.jpg"
+  },
+  {
+    "name": "Converse Chuck Taylor",
+    "price": 69,
+    "size": 'XL',
+    "imageUrl": "images/model.jpg"
+  },
+  {
+    "name": "Vans Old Skool",
+    "price": 79,
+    "size": 'L',
+    "imageUrl": "images/model.jpg"
+  },
+  {
+    "name": "Reebok Classic Leather",
+    "price": 89,
+    "size": 'L',
+    "imageUrl": "images/model.jpg"
+  },
+  {
+    "name": "Reebok Leather",
+    "price": 189,
+    "size": 'XXL',
+    "imageUrl": "images/model.jpg"
+  },
+  {
+    "name": "Reebokok Classic Leather",
+    "price": 70,
+    "size": 'M',
+    "imageUrl": "images/model.jpg"
+  },
+  {
+    "name": "Leather jacket",
+    "price": 120,
+    "size": 'L',
+    "imageUrl": "images/model.jpg"
+  }
+];
 }
