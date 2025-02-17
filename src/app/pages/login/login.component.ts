2dfb29c6f39b4d17c8cd9b65b94027436a9d91b9
Mahmoud Elkhayat <Elkhayat.mahmoud@outlook.com>
2025-02-17 11:58:43 +0200

    login service

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../core/services/auth.service';
+
 
 @Component({
   selector: 'app-login',
@@ -8,5 +10,21 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
-
+//   {
+//     username: "mor_2314",
+//     password: "83r5^_"
+// }
+  constructor(private authService:AuthService) { }
+  data: {username:string,password:string} | null= null;
+  loginCall(){
+    this.authService.login(this.data).subscribe({
+   //success
+      next: (res:any)=> {
+        console.log("result:",res);
+      },
+      error: ()=> {},
+      complete: ()=> {}
+    })
+  }
 }
+
